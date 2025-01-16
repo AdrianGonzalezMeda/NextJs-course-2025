4a32c9deb35f0cb24dd603a50ebb19e0592926a1
Adrian Gonzalez Meda <adriangonzalezmeda@gmail.com>
2025-01-16 15:38:53 +0100

    handling not found pages

--- 2 Routing and Page Rendering/1 Starting Project/app/news/page.js
+++ 2 Routing and Page Rendering/1 Starting Project/app/news/page.js
@@ -1,17 +1,21 @@
 import Link from "next/link"
 
+import { DUMMY_NEWS } from "@/dummy-news"
+
 const NewsPage = () => {
     return (
         <>
             <h1>News</h1>
-            
+
             <ul className='news-list'>
-                <li>
-                    <Link href='/news/abc'>Abc</Link>
-                </li>
-                <li>
-                    <Link href='/news/abcd'>Abcd</Link>
-                </li>
+                {DUMMY_NEWS.map(newsItem => (
+                    <li key={newsItem.id}>
+                        <Link href={`/news/${newsItem.slug}`}>
+                            <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+                            <span>{newsItem.title}</span>
+                        </Link>
+                    </li>
+                ))}
             </ul>
         </>
     )
